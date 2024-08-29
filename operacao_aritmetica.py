a5aa2eb07c2176ef6a7bbee8dbc9aa5cc46b57f4
scarieldias <alessandro.scariel@gmail.com>
2024-08-28 21:27:38 -0300

    Operacoes Aritmetica

---
+++ operacao_aritmetica.py
@@ -0,0 +1,32 @@
+a = 5
+b = 3
+
+# adicao
+
+resultado = a + b
+print(f"Soma: {resultado}")
+
+# Subtracao
+
+resultado = a - b
+print(f"Subtracao: {resultado}")
+
+# Multiplicacao
+
+resultado = a * b
+print(f"Multiplicacao: {resultado}")
+
+# Divisao
+
+resultado = a // b
+print(f"Divisao Inteira: {resultado}")
+
+# Resto
+
+resultado = a % b
+print(f"Resto: {resultado}")
+
+# Exponencial
+
+resultado = a ** b
+print(f"Exponencial: {resultado}")
